4066ca330b5f45535ffcfcc257ef5e8bba9f4869
arnadu89 <arnadu89@gmail.com>
2023-07-24 10:51:28 +0200

    config.yml and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM python:3.10-slim
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-RUN mkdir /app
 WORKDIR /app
 RUN pip install --upgrade pip
 COPY requirements.txt /app/
--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import pytest
+from pytest_mock import mocker
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_letting_index():
+    client = Client()
+
+    url = reverse("lettings_index")
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+
+# @pytest.mark.django_db
+# def test_letting(mocker):
+#     client = Client()
+#
+#     # mocked_letting = ""
+#     #
+#     # mocker.patch("", return_value=mocked_letting)
+#
+#     url = reverse("letting", kwargs={"letting_id": "1"})
+#     response = client.get(url)
+#
+#     assert response.status_code == 200
