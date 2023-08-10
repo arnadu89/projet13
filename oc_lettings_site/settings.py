6339a177ff11370ddc807d4f8cbc81cb9f4fc9b1
arnadu89 <arnadu89@gmail.com>
2023-08-10 13:16:02 +0200

    update static collect

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -117,16 +117,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 sentry_sdk.init(
     dsn=os.environ.get('SENTRY_DSN'),
